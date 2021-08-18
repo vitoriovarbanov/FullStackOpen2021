fafdeafc87d43df3bdd1a4066d52020cfc33c055
Vitorio Varbanov <vitorio.0405@hotmail.com>
2021-08-18 14:15:27 +0300

    part 1 - all exercises submitted

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+/* import React, { useState } from 'react';
 import Button from './components/Button';
 import Content from './components/Content';
 import Statistics from './components/Statistics';
@@ -8,7 +8,7 @@ const App = () => {
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
-  /* const onFeedbackGiven = (gradeReceived) => {
+  const onFeedbackGiven = (gradeReceived) => {
     const handler = () => {
       if (gradeReceived === 'good') {
         setGood(good + 1)
@@ -21,7 +21,7 @@ const App = () => {
 
     return handler
   }
- */
+
 
   return (
     <>
@@ -44,6 +44,68 @@ const App = () => {
           </>
       }
 
+    </>
+  );
+} */
+
+
+//ANECDOTES
+import React, { useState } from 'react';
+
+
+const App = () => {
+  const anecdotes = [
+    'If it hurts, do it more often',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients'
+  ]
+
+  const [selected, setSelected] = useState(0);
+  const [votes, setVotes] = useState({ 0: 0, 1: 0, 2: 0, 3: 0, 4: 0, 5: 0, 6: 0 });
+
+  let randomNumber = Math.random() * 6;
+
+  const selectNum = () => {
+    const handler = () => {
+      setSelected(Number(randomNumber.toFixed(0)))
+    }
+    return handler
+  }
+
+  const updateVotes = () => {
+    const handler = () => {
+      const copy = { ...votes }
+      console.log(copy)
+      copy[selected] += 1
+      console.log(copy)
+      setVotes(copy)
+    }
+    return handler
+  }
+
+  const showBestAnecdote = () => {
+    const votesArray = Object.values(votes)
+    let maxVotes = Math.max(...votesArray)
+    let index = votesArray.indexOf(maxVotes)
+    return anecdotes[index]
+  }
+
+
+  return (
+    <>
+      <h1>Anecdote of the day</h1>
+      <p>{anecdotes[selected]}</p>
+      <p>has {votes[selected]} votes</p>
+      <div>
+        <button onClick={updateVotes()}>Vote</button>
+        <button onClick={selectNum()}>Next Anecdote</button>
+      </div>
+      <h1>Anecdote with most votes</h1>
+      <p>{showBestAnecdote()}</p>
     </>
   );
 }
