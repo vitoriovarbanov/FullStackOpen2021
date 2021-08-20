085cd0c13331e0cb8ea3da462de69f14b51b7241
Vitorio Varbanov <vitorio.0405@hotmail.com>
2021-08-20 16:56:42 +0300

    class tasks part 3

---
+++ part3/exercises/index.js
@@ -0,0 +1,60 @@
+const express = require('express')
+const app = express()
+
+app.use(express.json())
+
+let notes = [
+    {
+        id: 1,
+        content: "HTML is easy",
+        date: "2019-05-30T17:30:31.098Z",
+        important: true
+    },
+    {
+        id: 2,
+        content: "Browser can execute only Javascript",
+        date: "2019-05-30T18:39:34.091Z",
+        important: false
+    },
+    {
+        id: 3,
+        content: "GET and POST are the most important methods of HTTP protocol",
+        date: "2019-05-30T19:20:14.298Z",
+        important: true
+    }
+]
+
+
+app.get('/', (req, res) => {
+    res.send(`WELCOMEE`)
+})
+
+app.get('/notes', (req, res) => {
+    res.json(notes)
+})
+
+app.get('/api/notes/:id', (req, res) => {
+    const id = Number(req.params.id)
+    const note = notes.find(note => note.id === id)
+    if (note) {
+        res.json(note)
+    } else {
+        res.send(400).end()
+    }
+})
+
+app.post('/api/notes', (req, res) => {
+    const note = req.body
+    res.json(note)
+})
+
+app.delete('/api/notes/:id', (req, res) => {
+    const id = Number(req.params.id)
+    notes = notes.filter(note => note.id !== id)
+
+    res.status(204).end()
+})
+
+const PORT = 3001
+app.listen(PORT)
+console.log(`Server listening on port ${PORT}`)
