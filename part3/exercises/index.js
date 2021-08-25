9a86f9306cd8a353436677a7c9daaa28bc993716
Vitorio Varbanov <vitorio.0405@hotmail.com>
2021-08-25 17:34:50 +0300

    3.1 - 3.6 finished

--- part3/exercises/index.js
+++ part3/exercises/index.js
@@ -1,58 +1,88 @@
 const express = require('express')
 const app = express()
+var moment = require('moment')
+var format = 'HH:mm:ss';
 
 app.use(express.json())
 
-let notes = [
+let people = [
     {
-        id: 1,
-        content: "HTML is easy",
-        date: "2019-05-30T17:30:31.098Z",
-        important: true
+        "id": 1,
+        "name": "Arto Hellas",
+        "number": "040-123456"
     },
     {
-        id: 2,
-        content: "Browser can execute only Javascript",
-        date: "2019-05-30T18:39:34.091Z",
-        important: false
+        "id": 2,
+        "name": "Ada Lovelace",
+        "number": "39-44-5323523"
     },
     {
-        id: 3,
-        content: "GET and POST are the most important methods of HTTP protocol",
-        date: "2019-05-30T19:20:14.298Z",
-        important: true
+        "id": 3,
+        "name": "Dan Abramov",
+        "number": "12-43-234345"
+    },
+    {
+        "id": 4,
+        "name": "Mary Poppendieck",
+        "number": "39-23-6423122"
     }
 ]
-
 
 app.get('/', (req, res) => {
     res.send(`WELCOMEE`)
 })
 
-app.get('/notes', (req, res) => {
-    res.json(notes)
+app.get('/api/people', (req, res) => {
+    res.json(people)
 })
 
-app.get('/api/notes/:id', (req, res) => {
+app.get('/info', (req, res) => {
+    var timestamps = moment().format()
+    const html = `
+    Phonebook has info for ${people.length} people<br><br>
+    ${timestamps}
+    `
+    res.send(html)
+})
+
+app.get('/api/people/:id', (req, res) => {
     const id = Number(req.params.id)
-    const note = notes.find(note => note.id === id)
-    if (note) {
-        res.json(note)
+    const person = people.find(x => x.id === id)
+    if (person) {
+        res.json(person)
     } else {
         res.send(400).end()
     }
 })
 
-app.post('/api/notes', (req, res) => {
-    const note = req.body
-    res.json(note)
+app.delete('/api/people/:id', (req, res) => {
+    const id = Number(req.params.id)
+    people = people.filter(x => x.id !== id)
+    res.send(204).end()
 })
 
-app.delete('/api/notes/:id', (req, res) => {
-    const id = Number(req.params.id)
-    notes = notes.filter(note => note.id !== id)
+app.post('/api/people', (req, res) => {
+    const id = Math.random().toString(36).slice(2);
+    const body = req.body
+    console.log(body.content)
 
-    res.status(204).end()
+    if (!body.hasOwnProperty("name")) {
+        return res.status(400).json({
+            error: 'content missing'
+        })
+    }
+
+    const newPerson = { ...req.body, id }
+
+    const duplicatedName = people.find(x => x.name === newPerson.name)
+    if (duplicatedName) {
+        return res.status(400).json({
+            error: 'The name is already existing in the phonebook!'
+        })
+    }
+
+    people = people.concat(newPerson)
+    res.json(newPerson)
 })
 
 const PORT = 3001
