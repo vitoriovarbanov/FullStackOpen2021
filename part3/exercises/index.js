d0ff87996150f4f9ade08427a76cf295e6d03dd2
Vitorio Varbanov <vitorio.0405@hotmail.com>
2021-08-27 11:56:06 +0300

    heroku added

--- part3/exercises/index.js
+++ part3/exercises/index.js
@@ -96,6 +96,7 @@ app.post('/api/people', (req, res) => {
     res.json(newPerson)
 })
 
-const PORT = 3001
-app.listen(PORT)
-console.log(`Server listening on port ${PORT}`)
+const PORT = process.env.PORT || 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
