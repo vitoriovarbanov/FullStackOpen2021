bddf476a9a4bf68c9d2a20774f47323bae5070a8
Vitorio Varbanov <vitorio.0405@hotmail.com>
2021-08-27 14:10:45 +0300

    tt

--- part3/exercises/index.js
+++ part3/exercises/index.js
@@ -96,6 +96,7 @@ app.post('/api/people', (req, res) => {
     res.json(newPerson)
 })
 
+
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
